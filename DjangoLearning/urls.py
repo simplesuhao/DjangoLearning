3c0d0d0db1cbdd872e57e8057699531027e3d986
suhao <simplesuhao@163.com>
2016-09-11 23:29:13 +0800

    Last Lesson

--- DjangoLearning/urls.py
+++ DjangoLearning/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from new_test import add
+from new_test import csv_test
 from new_test import helloworld
 from new_test import list
 urlpatterns = [
+    url(r'^csv/(?P<filename>\w+)/$', csv_test.output),
     url(r'^list/$', list.index),
     url(r'^$', helloworld.index),
     url(r'^add/$', add.index),
